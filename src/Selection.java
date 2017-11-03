7aa7d23ae90aada8e51d350319464f5d3992c3cf
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-11-03 10:55:12 +0100

    Initialize Selection.sort

--- src/Selection.java
+++ src/Selection.java
@@ -3,4 +3,11 @@
  * Created by IF on 03.11.17.
  */
 public class Selection {
+    /**
+     * Sort method
+     * @param a Array of Comparables
+     */
+    public static void sort(Comparable[] a) {
+
+    }
 }
