567ba6dc8076d0a7b1e50a38d653452585c62f1b
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-11-03 11:03:55 +0100

    Initialize Selection.exch

--- src/Selection.java
+++ src/Selection.java
@@ -13,11 +13,25 @@ public class Selection {
 
     /**
      * Less helper function
-     * @param v
-     * @param w
-     * @return
+     * @param v Comparable object to be compared to
+     * @param w Comparable object to be compared
+     * @return Boolean true if v < w, false otherwise
      */
     private static boolean less(Comparable v, Comparable w) {
         return v.compareTo(w) < 0;
     }
+
+    /**
+     * Exchange helper function
+     * @param a Array of Comparables to which the objects to be exchanged belong
+     * @param i Int array index of first object to be exchanged
+     * @param j Int array index of second object to be exchanged
+     */
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+
 }
