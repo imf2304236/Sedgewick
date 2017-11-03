8f0df60dce139a68327fc424cca61f96e217224f
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-11-03 11:21:47 +0100

    Write initial Selection.sort algorithm

--- src/Selection.java
+++ src/Selection.java
@@ -8,7 +8,16 @@ public class Selection {
      * @param a Array of Comparables
      */
     public static void sort(Comparable[] a) {
-
+        int N = a.length;
+        for (int i = 1; i < N; i++) {
+            int min = i;
+            for (int j = i+1; j < N; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exch(a, i, min);
+        }
     }
 
     /**
@@ -57,8 +66,4 @@ public class Selection {
         }
         System.out.println();
     }
-
-
-
-
 }
