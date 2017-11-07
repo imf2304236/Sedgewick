b08fd92991d27d9849bd771e6dafa53f02f24207
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-11-07 18:29:57 +0100

    Fix final index bug in Insertion.sort()

--- src/Insertion.java
+++ src/Insertion.java
@@ -9,7 +9,7 @@ public class Insertion {
      */
     public static void sort(Comparable[] a) {
         int N = a.length;
-        for (int i = 1; i < N-1; i++) {
+        for (int i = 1; i < N; i++) {
             for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
                     exch(a, j, j-1);
             }
@@ -79,7 +79,7 @@ public class Insertion {
         a[7] = "seen";
 
         sort(a);
-        if (!isSorted(a)) throw new Error();
         show(a);
+        if (!isSorted(a)) throw new Error();
     }
 }
