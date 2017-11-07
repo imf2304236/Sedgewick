ea0894522ae4c7155f1f509a08699cc9c162f03e
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-11-07 18:59:58 +0100

    Remove refactored tests from Main class

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,6 @@
  */
 public class Main {
     public static void main(String[] args) {
-        Comparable[] test = new Comparable[9];
-        test[0] = "it";
-        test[1] = "it";
-        test[2] = "was";
-        test[3] = "best";
-        test[4] = "the";
-        test[5] = "of";
-        test[6] = "times";
-        test[7] = "ever";
-        test[8] = "seen";
 
-        Comparable[] selectionTest = test;
-        Comparable[] insertionTest = test;
-        Comparable[] mergeTest = test;
-
-        Selection.sort(selectionTest);
-        Insertion.sort(selectionTest);
-        Merge.sort(selectionTest);
     }
 }
