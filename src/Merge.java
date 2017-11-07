f8eba333e3af43421292949cf1b470acae5654d9
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-11-07 17:55:13 +0100

    Write inital Merge class with recursive merge & sort methods

--- src/Merge.java
+++ src/Merge.java
@@ -3,4 +3,105 @@
  * Created by IF on 07.11.17.
  */
 public class Merge {
+    private static Comparable[] temp;
+
+    /**
+     * Recursive sort method which creates a temp array and begins sorting
+     * @param a Array of Comparables
+     */
+    public static void sort(Comparable[] a) {
+        temp = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+    /**
+     * Recursive sort method which sorts both subarrays separately, then merges the two in place
+     * @param a Array of Comparables
+     * @param lo Integer of initial array index
+     * @param hi Integer of final array index
+     */
+    public static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) { return; }
+        int mid = lo + (hi - lo)/2;
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
+
+    /**
+     * Merge method which merges an array containing two partially sorted subarrays
+     * @param a Array of Comparables
+     * @param lo Integer of initial array index
+     * @param mid Integer of median array index
+     * @param hi Integer of final array index
+     */
+    private static void merge(Comparable[] a, int lo, int mid, int hi) {
+        int i = lo;         // Index of smallest member of left subarray
+        int j = mid + 1;    // Index of smallest member of right subarray
+
+        // Copy contents of a to temp from index lo to hi
+        for (int k = 0; k<a.length - 1; k++) {
+            temp[k] = a[k];
+        }
+
+        // Merge contents of a to temp
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {          // Copy contents of right array when left index passes mid index
+                a[k] = temp[j];
+                j++;
+            } else if (j > hi) {    // Copy contents of left array when right index passes hi index
+                a[k] = temp[i];
+                i++;
+            } else if (less(temp[j], temp[i])) {    // Compare smallest members of each array
+                a[k] = temp[j];
+                j++;
+            } else {
+                a[k] = temp[i];
+                i++;
+            }
+        }
+    }
+
+    /**
+     * Less helper method
+     * @param v Comparable object to be compared to
+     * @param w Comparable object to be compared
+     * @return Boolean true if v < w, false otherwise
+     */
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    /**
+     * Method which checks if an array is sorted
+     * @param a Array of Comparables to be checked
+     * @return Boolean true if the array is sorted, false otherwise
+     */
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i-1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Method which prints each member of the array on a single line
+     * @param a Array of Comparables to be printed
+     */
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Main method to run tests
+     * @param args Array of strings to define test behavior
+     */
+    public static void main(String[] args) {
+
+    }
 }
